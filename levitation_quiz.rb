fe4549a3fbdcde8ddc52654abba346489eb89d01
Shawna Davis <shawna_davis@alum.mit.edu>
2018-09-08 00:45:56 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,8 +1,11 @@
 
 def levitation_quiz
 	#your code here
-	loop do end 
-	  puts
+	loop do 
+	  puts "What is the spell that enacts levitation?"
+	  answer = gets.chomp
+	  break if answer ==
+	 end
 end
 
 
